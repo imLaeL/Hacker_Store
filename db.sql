adf08801de6dcda7598a1e9e32806a95adf35de9
imLaeL <isaquelael38@gmail.com>
2026-01-29 15:41:05 -0300

    delecao de arquivo desnecessário

--- db.sql
+++
@@ -1,8 +0,0 @@
-create table if not EXISTS produtos(
-  id integer PRIMARY KEY AUTOINCREMENT not NULL,
-  nome varchar(50) not NULL,
-  preco integer not NULL,
-  qtde integer not NULL
-);
-
-insert into produtos(nome, preco, qtde) VALUES ('Rubber ducky', 25000, 4);
